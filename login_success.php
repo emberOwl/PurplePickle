20ca7787663f1c2a379c5bfb14b90f581f71c3ef
gothicUnicorn <ln831@nyu.edu>
2015-11-20 11:40:33 -0500

    formatted login_success

--- login_success.php
+++ login_success.php
@@ -1,18 +1,19 @@
 <?php
-//session_start();
-//if(!session_is_registered(myusername)){
-if(!isset($_SESSION['myusername']) ){
-    header("location:index.html");
-    exit();
-}
+    //session_start();
+    //if(!session_is_registered(myusername)){
+    if(!isset($_SESSION['myusername']))
+    {
+        header("location:index.html");
+        exit();
+    }
 ?>
 <html>
-<head>
-<!--<meta http-equiv="refresh" content="1;url=http://purplepickle.byethost31.com/upload.html" />
--->
-</head>
-<body>
-Login successful!
-Redirecting in 3 seconds...
-</body>
+    <head>
+    <!--<meta http-equiv="refresh" content="1;url=http://purplepickle.byethost31.com/upload.html" />
+    -->
+    </head>
+    <body>
+        Login successful!
+        Redirecting in 3 seconds...
+    </body>
 </html>
